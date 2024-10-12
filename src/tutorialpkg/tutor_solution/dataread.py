c3affd10cb51d10dbcd07673530633bc3ffd5c8a
HanGao42 <zcemhg0@ucl.ac.uk>
2024-10-12 14:51:18 +0100

    activity2.2 finished

--- src/tutorialpkg/tutor_solution/dataread.py
+++ src/tutorialpkg/tutor_solution/dataread.py
@@ -15,6 +15,15 @@ def some_function(argument1):
 
 
 if __name__ == '__main__':
-    file_path = pathlib.Path(__file__).parent.parent.joinpath("data","paralympics_events_raw.csv")
-    df = pd.read_csv(file_path)
-    print(df.head())
+    file_path1 = pathlib.Path(__file__).parent.parent.joinpath("data","paralympics_events_raw.csv")
+    df1 = pd.read_csv(file_path1)
+    # print(df1.head())
+
+    file_path2 = pathlib.Path(__file__).parent.parent.joinpath("data","paralympics_all_raw.xlsx")
+    df2 = pd.read_excel(file_path2)
+    # print(df2.head())
+
+    df3 = pd.read_excel(file_path2, sheet_name = "medal_standings")
+    print(df3.head())
+
+
