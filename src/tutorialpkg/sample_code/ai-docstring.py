7fd39b9c327d0b9a4657efee104f87f280828650
HanGao42 <zcemhg0@ucl.ac.uk>
2024-10-12 14:09:48 +0100

    nothing

--- src/tutorialpkg/sample_code/ai-docstring.py
+++ src/tutorialpkg/sample_code/ai-docstring.py
@@ -1,15 +1,15 @@
 
-    """ Prints information that the describes the contents of the DataFrame.
+##    """ Prints information that the describes the contents of the DataFrame.
 
-      - Print the number of rows and columns in the DataFrame using `.shape`
-      - Print the first 7 rows of data using `.head()` and the last 6 rows using `.tail()`
-      - Print the column labels using `.info()`
-      - Print the column data types using `.dtypes`
-      - Print general statistics using `.describe()`
+##      - Print the number of rows and columns in the DataFrame using `.shape`
+##      - Print the first 7 rows of data using `.head()` and the last 6 rows using `.tail()`
+##      - Print the column labels using `.info()`
+##      - Print the column data types using `.dtypes`
+##      - Print general statistics using `.describe()`
 
-    Args:
-        df: A pandas dataframe containing the data
-    """
+##    Args:
+##        df: A pandas dataframe containing the data
+##    """
 
 
 
