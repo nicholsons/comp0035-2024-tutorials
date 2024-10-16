9005e942701bf4e550b565145ffd97d88102a629
HanGao42 <zcemhg0@ucl.ac.uk>
2024-10-16 19:39:31 +0100

    3.2 finished

--- src/tutorialpkg/tutor_solution/activity3.2.py
+++ src/tutorialpkg/tutor_solution/activity3.2.py
@@ -1,15 +1,20 @@
 import pandas as pd
-import matplotlib as plt
+import matplotlib.pyplot as plt
 from pathlib import Path
 
-target_file = "paralympics_events_raw.csv"
 
 def readcsv(target_file):
     # Filepath of the csv data file
     try:
-        datafile_csv = Path.Path(__file__).parent.parent.joinpath("data", target_file)
+        datafile_csv = Path(__file__).parent.parent.joinpath("data", target_file)
     except FileNotFoundError as e:
         print(f"File not found. Please check the file path. Error: {e}")
 
     # Read the data from the file into a Pandas dataframe
-    events_csv_df = pd.read_csv(datafile_csv)
+    return(pd.read_csv(datafile_csv))
+
+if __name__ == '__main__':
+    target_file = "paralympics_events_raw.csv"
+    df = readcsv(target_file)
+    df.hist()
+    plt.show()
