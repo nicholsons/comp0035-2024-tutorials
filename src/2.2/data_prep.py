b4309b8e52f86a18d7c3f18289b6f83d28846c5a
Alex Chen <2976554292@qq.com>
2024-10-19 21:34:38 +0100

    week2 task

--- src/2.2/data_prep.py
+++ src/2.2/data_prep.py
@@ -1,25 +1,49 @@
 import pathlib
 import pandas as pd
 
+dtype_dict = {
+    'column1': 'int64',
+    'column2': 'float64',
+    'column3': 'int64',
+    # Add more columns as needed
+}
 
-def some_function(argument1):
-     """Summary or Description of the Function
- 
-        Parameters:
-        argument1 (int): Description of arg1
- 
-        Returns:
-        int:Returning value
- 
-     """
+def describe_dataframe(events_csv_df):
+     # print(events_csv_df.shape)
+     # print(events_csv_df.head , events_csv_df.tail)
+     # print(events_csv_df.columns)
+     print(events_csv_df.dtypes)
+     # print(events_csv_df.info)
+     # print(events_csv_df.describe)
+
+def prepare_data(events_csv_df):
+     events_csv_df['start'] = pd.to_datetime(events_csv_df['start'], format='%d/%m/%Y')
+     events_csv_df['end'] = pd.to_datetime(events_csv_df['end'], format='%d/%m/%Y')
+     columns_to_change = ['countries', 'events', 'participants_m', 'participants_f', 'participants']
+     events_csv_df[columns_to_change] = events_csv_df[columns_to_change].fillna(0).astype(int)
+    
+     return events_csv_df
+
+
 if __name__ == '__main__':
      pth = pathlib.Path(__file__)
-     print(pth)
-     paralympics_datafile_excel = pth.parent.joinpath("tutorialpkg").joinpath("data", "paralympics_all_raw.xlsx")
-     paralympics_events_raw_csv = pth.parent.joinpath("tutorialpkg").joinpath("data", "paralympics_events_raw.csv")
-     if(paralympics_datafile_excel.exists):
-          print("file exists")
-     else:
-          print("file not exists")
-     pd.read_excel(paralympics_datafile_excel)
- 
+     try:
+          paralympics_datafile_csv = pth.parent.parent / 'tutorialpkg' / 'data' / 'paralympics_events_raw.csv'
+          path_to_npc_csv_file = pth.parent.parent /'tutorialpkg/data/npc_codes.csv'
+          # cols=['type', 'year', 'country', 'host', 'start', 'end', 'countries', 'events', 'sports','participants_m', 'participants_f', 'participants']
+          npc_codes_df = pd.read_csv(path_to_npc_csv_file, usecols=['Name', 'Code'], encoding='utf-8', encoding_errors='ignore')
+     except FileNotFoundError as e:
+          print(f"File not found. Please check the file path. Error: {e}")
+
+     # Read the data from the file into a Pandas dataframe
+     events_csv_df = pd.read_csv(paralympics_datafile_csv)
+     prepare_data(events_csv_df)
+     describe_dataframe(events_csv_df)
+     # merged_df = events_csv_df.merge(npc_codes_df, how='left', left_on='country', right_on='Name')
+     # print(merged_df[['country', 'Code', 'Name']])
+
+     
+     
+
+
+
